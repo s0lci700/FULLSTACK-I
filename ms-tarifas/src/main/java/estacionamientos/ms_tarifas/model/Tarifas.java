8b643fbda6f36dcf3b1640e98141341ec0c074fb
catalina <catalinaguirretorres@gmail.com>
2026-05-10 01:05:41 -0400

    agregue model y repo

---
+++ ms-tarifas/src/main/java/estacionamientos/ms_tarifas/model/Tarifas.java
@@ -0,0 +1,34 @@
+package estacionamientos.ms_tarifas.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data @Entity
+@Table(name = "tarifa")
+
+public class Tarifas {
+    @Id 
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique = true)
+    private String nombre;
+
+    @Column(nullable=true)
+    private String descripcion;
+
+    @Column(name= "precio_base_hora", nullable = false)
+    private Double precioBaseHora;
+
+    @Column(nullable= false)
+    private Boolean activo;
+}   
---
+++ ms-tarifas/src/main/java/estacionamientos/ms_tarifas/repository/HorarioTarifaRepository.java
@@ -0,0 +1,11 @@
+package estacionamientos.ms_tarifas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import estacionamientos.ms_tarifas.model.HorarioTarifas;
+
+@Repository
+public interface HorarioTarifaRepository extends JpaRepository<HorarioTarifas, Long> {
+
+}
