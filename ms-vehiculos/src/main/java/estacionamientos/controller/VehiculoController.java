b671235c3d883e9729d6cae93cbfb6dc1e0ba27a
catalina <catalinaguirretorres@gmail.com>
2026-05-11 00:18:48 -0400

    cambios cata

--- ms-vehiculos/src/main/java/estacionamientos/controller/VehiculoController.java
+++ ms-vehiculos/src/main/java/estacionamientos/controller/VehiculoController.java
@@ -2,14 +2,12 @@ package estacionamientos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import estacionamientos.service.VehiculoService;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 // TODO: Cambiar a "/api/vehiculos" (plural) — convención REST para colecciones.
