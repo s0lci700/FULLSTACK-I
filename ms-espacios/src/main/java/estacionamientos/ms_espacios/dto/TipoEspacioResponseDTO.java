8fa6b8a62f0f843de472ca0a5e8a4f5c7edc7fe1
catalina <catalinaguirretorres@gmail.com>
2026-06-28 23:46:19 -0400

    docs(swagger): agregar @Schema a DTOs de ms-espacios

--- ms-espacios/src/main/java/estacionamientos/ms_espacios/dto/TipoEspacioResponseDTO.java
+++ ms-espacios/src/main/java/estacionamientos/ms_espacios/dto/TipoEspacioResponseDTO.java
@@ -2,17 +2,40 @@ package estacionamientos.ms_espacios.dto;
 
 import java.math.BigDecimal;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-
+/**
+ * DTO de respuesta con los datos de un tipo de espacio
+ * Consumido como parte de EspacioResponseDTO
+ */
+@Data @AllArgsConstructor @NoArgsConstructor
+@Schema(description = "Datos de un tipo de espacio de estacionamiento")
 public class TipoEspacioResponseDTO {
+
+    @Schema(
+        description = "ID único del tipo de espacio",
+        example = "1"
+    )
     private Long id;
+
+    @Schema(
+        description = "Nombre descriptivo del tipo",
+        example = "Estándar"
+    )
     private String nombre;
+
+    @Schema(
+        description = "Descripción adicional",
+        example = "Espacio para autos particulares"
+    )
     private String descripcion;
+
+    @Schema(
+        description = "Factor multiplicador de precio para este tipo",
+        example = "1.0"
+    )
     private BigDecimal factorPrecio;
 }
