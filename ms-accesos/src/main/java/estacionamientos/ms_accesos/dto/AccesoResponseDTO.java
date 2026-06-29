c379e425898f8ffa79b87aff8b4168de775041a6
Catalina Aguirre <ca.aguirret@duocuc.cl>
2026-06-29 00:49:07 -0400

    docs(swagger): agregar @Schema a DTOs de ms-accesos

--- ms-accesos/src/main/java/estacionamientos/ms_accesos/dto/AccesoResponseDTO.java
+++ ms-accesos/src/main/java/estacionamientos/ms_accesos/dto/AccesoResponseDTO.java
@@ -1,6 +1,7 @@
 package estacionamientos.ms_accesos.dto;
 
 import java.time.LocalDateTime;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,14 +12,18 @@ public class AccesoResponseDTO {
 
     private Long id;
 
+    @Schema(description = "ID del vehículo que ingresa", example = "1")
     private Long idVehiculo;
 
     private Long idEspacio;
 
+    @Schema(description = "ID de la reserva asociada", example = "1")
     private Long idReserva;
 
+    @Schema(description = "Patente escaneada en la barrera", example = "ABCD12")
     private String patenteEscaneada;
 
+    @Schema(description = "Fecha y hora de entrada", example = "2026-06-29T12:30:00")
     private LocalDateTime fechaHoraEntrada;
 
     private LocalDateTime fechaHoraSalida;
